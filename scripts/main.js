81183dd656f753ab86416900713513c78397275c
masato-sue <138498078+masato-sue@users.noreply.github.com>
2024-01-20 17:56:19 +0900

    htmlにイベントを追加

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent ="Hello world!";
+
+document.querySelector("html").addventListener("click",function(){
+    alert("痛っ！つつかないで！");
+});
