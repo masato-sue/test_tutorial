16a0cdb68b24981d48ee8f459a5def2935b84924
masato-sue <138498078+masato-sue@users.noreply.github.com>
2024-01-20 18:11:04 +0900

    imageの切り替えの挿入

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const myImage=document.querySelector("img");
+
+myImage.onclick=()=>{
+    const mySrc=myIMage.getAttribute("src");
+    if(mySrc==="images/pngimg.com - github_PNG53.png"){
+        myImage.setAttribute("src","images/purepng.com-cat-cuteanimalscats-115205020723cicy.png");
+    }else{
+        myImage.setAttribute("src","images/pngimg.com - github_PNG53.png");
+    }
+};
