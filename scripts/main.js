277e4f0c88c0c54704b0ba99a7a9adeab17a9a1a
masato-sue <138498078+masato-sue@users.noreply.github.com>
2024-01-20 18:13:10 +0900

    myImageの修正

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const myImage=document.querySelector("img");
 
 myImage.onclick=()=>{
-    const mySrc=myIMage.getAttribute("src");
+    const mySrc=myImage.getAttribute("src");
     if(mySrc==="images/pngimg.com - github_PNG53.png"){
         myImage.setAttribute("src","images/purepng.com-cat-cuteanimalscats-115205020723cicy.png");
     }else{
