c4e31a9e2e284c3a3eb71f439caeeb6248751aa3
masato-sue <138498078+masato-sue@users.noreply.github.com>
2024-01-20 18:41:53 +0900

    イベントの追加

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,25 @@ myImage.onclick=()=>{
         myImage.setAttribute("src","images/pngimg.com - github_PNG53.png");
     }
 };
+
+let myButton=document.querySelector("button");
+let myHeading=document.querySelector("h1");
+
+function setUserName(){
+    const myName=prompt("あなたの名前を入力してください。");
+    if(!myName){
+        setUserName();
+    }else{
+    localStorage.setItem("name",myName);
+    myHeading.textContent=`${myName}さん、github使ってる？`;
+    }
+}
+
+if (!localStorage.getItem("name")){
+    setUserName();
+}else{
+    const storedName=localStorage.getItem("name");
+    myHeading.textContent=`${storedName}さん、githubにはもう慣れた？`;
+}
+
+myButton.onclick=setUserName();
